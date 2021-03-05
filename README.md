84666f27bb7cbdf8ab54395419df9bcc84c0db52
Constantin Pape <constantin.pape@embl.de>
2021-03-05 16:34:47 +0100

    Add project creation script

---
+++ README.md
@@ -0,0 +1,1 @@
+# zebrafish-lm-datasets
---
+++ create_project.py
@@ -0,0 +1,166 @@
+import json
+import os
+import re
+from glob import glob
+
+from mobie import initialize_dataset, add_image_data, add_segmentation
+
+ROOT = "/g/schwab/Kimberly/Publications/MoBIE_paper/Jonas_project/raw/idr_0079"
+MODALITIES = {"experimentA": "membrane",
+              "experimentB": "nuclei",
+              "experimentC": "actin",
+              "experimentD": "cisgolgi",
+              "experimentE": "early_endosomes",
+              "experimentF": "recycling_endosomes",
+              "experimentG": "tgn_and_late_endosomes",
+              "experimentH": "trans_golgi",
+              "experimentI": "atoh1a",
+              "experimentJ": "lysosomes"}
+
+
+def create_sample(experiment, sample, sample_dir,
+                  is_default=False, dry_run=True):
+    pattern = re.compile(f"({sample}_8bit_)(\w+)")
+
+    unit = "micrometer"
+    chunks = (64, 64, 64)
+    scale_factors = 4 * [[2, 2, 2]]
+
+    #
+    # initialize the dataset with '...lynEGFP.tif'
+    #
+    im_path = glob(os.path.join(sample_dir, '*lynEGFP.tif'))
+    assert len(im_path) == 1
+    im_path = im_path[0]
+    im_name = os.path.split(im_path)[1]
+
+    mobie_name = 'membrane-' + pattern.match(im_name).group(2)
+
+    txt_file = im_path.replace('.tif', '.txt')
+    with open(txt_file) as f:
+        res = f.readline().rstrip()
+        res = list(map(float, res.split(',')))[::-1]
+
+    if dry_run:
+        print("Initialize dataset:")
+        print(im_path)
+        print(mobie_name, res)
+    else:
+        initialize_dataset(
+            input_path=im_path,
+            input_key='',
+            root='./data',
+            dataset_name=sample,
+            raw_name=mobie_name,
+            resolution=res,
+            scale_factors=scale_factors,
+            chunks=chunks,
+            unit=unit,
+            target='local',
+            max_jobs=16,
+            is_default=is_default
+        )
+
+    #
+    # add the segmentation with '...lynEGFP_seg.tif'
+    #
+    seg_path = glob(os.path.join(sample_dir, '*seg.tif'))
+    assert len(seg_path) == 1, f"{seg_path}"
+    seg_path = seg_path[0]
+    seg_name = os.path.split(seg_path)[1]
+    mobie_name = 'membrane-' + pattern.match(seg_name).group(2)
+
+    txt_file = seg_path.replace('.tif', '.txt')
+    with open(txt_file) as f:
+        res = f.readline().rstrip()
+        res = list(map(float, res.split(',')))[::-1]
+
+    if dry_run:
+        print("Add seg:")
+        print(seg_path)
+        print(mobie_name, res)
+    else:
+        add_segmentation(
+            input_path=seg_path,
+            input_key='',
+            root='./data',
+            dataset_name=sample,
+            segmentation_name=mobie_name,
+            scale_factors=scale_factors,
+            resolution=res,
+            chunks=chunks,
+            unit=unit,
+            target='local',
+            max_jobs=16
+        )
+
+    #
+    # add all other images
+    #
+    additional_paths = glob(os.path.join(sample_dir, '*.tif'))
+    for path in additional_paths:
+        if path == im_path or path == seg_path:
+            continue
+
+        image_name = os.path.split(path)[1]
+        mobie_name = MODALITIES[experiment] + '-' + pattern.match(image_name).group(2)
+
+        txt_file = seg_path.replace('.tif', '.txt')
+        with open(txt_file) as f:
+            res = f.readline().rstrip()
+            res = list(map(float, res.split(',')))[::-1]
+
+        if dry_run:
+            print("Add additional image:")
+            print(path)
+            print(mobie_name, res)
+        else:
+            add_image_data(
+                input_path=path,
+                input_key='',
+                root='./data',
+                dataset_name=sample,
+                image_name=mobie_name,
+                scale_factors=scale_factors,
+                resolution=res,
+                chunks=chunks,
+                unit=unit,
+                target='local',
+                max_jobs=16
+            )
+
+
+def create_project(dry_run):
+
+    ds_path = './data/datasets.json'
+    if os.path.exists(ds_path):
+        with open(ds_path) as f:
+            datasets = json.load(f)['datasets']
+    else:
+        datasets = []
+
+    experiments = os.listdir(ROOT)
+    is_default = True
+    for exp in experiments:
+        exp_dir = os.path.join(ROOT, exp)
+        if not os.path.isdir(exp_dir):
+            print("Not a folder", exp_dir)
+            continue
+        sample = os.listdir(exp_dir)[0]
+
+        # skip creating this sample if it already exists
+        if sample in datasets:
+            print("Dataset", sample, "has already been created and is skipped")
+            is_default = False
+            continue
+
+        sample_dir = os.path.join(exp_dir, sample)
+        print("Creating sample:", sample)
+        create_sample(exp, sample, sample_dir,
+                      is_default=is_default, dry_run=dry_run)
+        print()
+        is_default = False
+
+
+if __name__ == '__main__':
+    create_project(dry_run=True)
